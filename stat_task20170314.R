09a1d86aa5b1a6595f841c45f11195d216629f6d
David MT Wong <mingtak.wong1986@gmail.com>
2017-03-13 23:02:04 +0800

    Fix a bug and removed testing code. Ready to be pushed to remote repo

--- stat_task20170314.R
+++ stat_task20170314.R
@@ -39,21 +39,24 @@ df[, 3:24] <- lapply(df[, 3:24], fix_dot)
 typeof(df);class(df);summary(df)
 
 # Testing on adding statistical summary using variable 'InfMort' before generalizing 
-ix <- !is.na(df['InfMort'])
 
-InfMort_tab <- ddply(df[ix, 1:3], "CountryName", summarise,
-                     begin = min(Year),
-                     end   = max(Year),
-                     duration = end-begin+1,
-                     check_big = max(InfMort)
+subset_data <- lapply(var1[3:24], function(x){
+  df[!is.na(df[x]), c('CountryName','Year',x)]
+}
 )
 
-summary_InfMort <- summarise(InfMort_tab,
-                             eariest = min(begin), 
-                             latest = max(end),
-                             tot_obs = sum(duration)
+lapply(subset_data, function(x) x[1,1:3])
+All_tab <- lapply(subset_data, ddply, "CountryName", summarise,
+                                      begin = min(Year),
+                                      end   = max(Year),
+                                      duration = length(Year)
 )
 
-# QA Checking
-InfMort_tab[c(1:5,100,184),]
-summary_InfMort
+
+summary_all <- lapply(All_tab, summarise, eariest = min(begin), 
+                                          latest = max(end),
+                                          tot_obs = sum(duration)
+)
+
+
+
