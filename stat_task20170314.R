1edf557f25bab4e474a0baf93d627ff498d11f12
MTDMTD <davidww86@hotmail.com>
2017-03-14 21:06:23 +0800

    Output table to excel

--- stat_task20170314.R
+++ stat_task20170314.R
@@ -36,37 +36,39 @@ fix_dot <- function(vari){
 }
 
 df[, 3:24] <- lapply(df[, 3:24], fix_dot)
-typeof(df);class(df);summary(df)
+#typeof(df);class(df);summary(df)
 
 # Testing on adding statistical summary using variable 'InfMort' before generalizing 
-ix <- !is.na(df['InfMort'])
+#ix <- !is.na(df['InfMort'])
 
-InfMort_tab <- ddply(df[ix, 1:3], "CountryName", summarise,
-                     begin = min(Year),
-                     end   = max(Year),
-                     duration = end-begin+1,
-                     check_big = max(InfMort)
-)
+#InfMort_tab <- ddply(df[ix, 1:3], "CountryName", summarise,
+#                     begin = min(Year),
+#                     end   = max(Year),
+#                     duration = end-begin+1,
+#                     check_big = max(InfMort)
+#)
 
-summary_InfMort <- summarise(InfMort_tab,
-                             eariest = min(begin), 
-                             latest = max(end),
-                             tot_obs = sum(duration)
-)
+#summary_InfMort <- summarise(InfMort_tab,
+#                             eariest = min(begin), 
+#                             latest = max(end),
+#                             tot_obs = sum(duration)
+#)
 
 # QA Checking
-InfMort_tab[c(1:5,100,184),]
-summary_InfMort
+#InfMort_tab[c(1:5,100,184),]
+#summary_InfMort
 
 subset_data <- lapply(var1[3:24], function(x){
   df[!is.na(df[x]), c('CountryName','Year',x)]
-})
+}
+)
 #subset_data[[1]][1:5,];subset_data[[2]][1:5,];subset_data[[22]][1:5,]
+#lapply(subset_data, function(x) x[1,])
 
 All_tab <- lapply(subset_data, ddply, "CountryName", summarise,
                   begin = min(Year),
                   end   = max(Year),
-                  duration = end-begin+1
+                  duration = length(Year)
 )
 
 #all(InfMort_tab[,1:4] == All_tab[[1]])
@@ -76,3 +78,8 @@ summary_all <- lapply(All_tab, summarise, eariest = min(begin),
 )
 
 #all(summary_InfMort[] == summary_all[[1]])
+
+summary_all <- do.call(rbind.data.frame, summary_all)
+row.names(summary_all) <-  var1[3:24]
+
+write.csv(summary_all, file = "summary_table.csv", row.names = T)
